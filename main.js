8714242fe8a0d3bbd6eadf7109793dafb314cdc1
Rishabh kumar <115525981+Rishabhvcm7257@users.noreply.github.com>
2024-07-11 09:56:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new type(".text",
+                  {
+                                    Strings:["Frontend Developer", "Web developer"],
+                                    typespeed:100,
+                                    backspeed:100,
+                                    backDelay:1000,
+                                    loop:true
+                  });
